fa67722e3a5ba17a4d089b6def4ad44211872366
Jana <janadahlmanns@gmail.com>
2025-11-01 18:54:07 +0100

    Koch snowflake animation done, that was easier than expected

---
+++ koch.py
@@ -0,0 +1,58 @@
+from manim import *
+import numpy as np
+
+class Koch(Scene):
+    def construct(self):
+        side_length = 10
+        max_depth = 5
+
+        snowflakes = []
+        for depth in range(max_depth + 1):
+            triangle = self.initial_triangle(side_length)
+            points = self.koch_recursive(triangle, depth)
+            shape = VMobject(stroke_color="#AFCBCF", stroke_width=1)
+            shape.set_points_as_corners(points + [points[0]])
+            snowflakes.append(shape)
+
+        current = snowflakes[0]
+        self.play(Create(current))
+        self.wait(1)
+
+        for next_shape in snowflakes[1:]:
+            self.play(Transform(current, next_shape))
+            self.wait(1)
+        
+        self.wait(2)
+
+    def initial_triangle(self, side_length):
+        height = side_length * np.sqrt(3) / 2
+        p1 = np.array([-side_length / 2, -height / 3, 0])
+        p2 = np.array([ side_length / 2, -height / 3, 0])
+        p3 = np.array([0, 2 * height / 3, 0])
+        return [p1, p2, p3]
+
+    def koch_recursive(self, points, depth):
+        if depth == 0:
+            return points
+        new_points = []
+        for i in range(len(points)):
+            p1 = points[i]
+            p2 = points[(i + 1) % len(points)]
+            new_points += self.add_bump(p1, p2)
+        return self.koch_recursive(new_points, depth - 1)
+
+    def add_bump(self, p1, p2):
+        v = p2 - p1
+        a = p1 + v / 3
+        b = p1 + 2 * v / 3
+        m = (a + b) / 2
+
+        dir = b - a
+        dir /= np.linalg.norm(dir)
+        normal = -np.array([-dir[1], dir[0], 0])
+
+        s = np.linalg.norm(b - a)
+        height = s * np.sqrt(3) / 2
+        peak = m + normal * height
+
+        return [p1, a, peak, b]
--- temp.py
+++
@@ -1,1 +0,0 @@
-Initial commit to connect to github
